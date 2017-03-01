9397bf99d342ebcfc469a9ee4ce090bcbc4a9176
Riccardo Venturini <riccardo@MBP-di-Rik.station>
2017-03-01 21:46:02 +0100

    removed foo and bar

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   scope :api, defaults: {format: :json}  do 
     resources :cities, except: [:new, :edit]
     resources :states, except: [:new, :edit]
-    resources :foos, except: [:new, :edit]
-    resources :bars, except: [:new, :edit]
- end      
+     end      
 
   get '/ui'  => 'ui#index'
   get '/ui#' => 'ui#index'
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
+City.delete_all
+State.delete_all
 cities = City.create(name: 'Baltimore')
 states = State.create(name: 'Maryland')
